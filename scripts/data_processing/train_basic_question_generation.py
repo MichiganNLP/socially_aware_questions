d28569ebb641ae81a856634bc7299c861fb36b37
Ian Stewart <ianbstew@lit1000.eecs.umich.edu>
2020-12-04 12:18:53 -0500

    added code for data cleaning

--- scripts/data_processing/train_basic_question_generation.py
+++ scripts/data_processing/train_basic_question_generation.py
@@ -16,7 +16,7 @@ import os
 from trainer import Trainer
 from data_helpers import DataProcessor, DataArguments
 
-def prepare_question_data(data, out_dir, tokenizer, train_pct=0.8):
+def prepare_question_data(data, out_dir, data_name, tokenizer, train_pct=0.8):
     # change to clean source/target format
     clean_data = data.loc[:, ['article_text', 'question']].rename(
         columns={'article_text': 'source_text', 'question': 'target_text'})
@@ -27,8 +27,8 @@ def prepare_question_data(data, out_dir, tokenizer, train_pct=0.8):
     np.random.shuffle(clean_data.values)
     clean_data_train = clean_data.iloc[:N_train, :]
     clean_data_val = clean_data.iloc[N_train:, :]
-    clean_data_train_out_file = os.path.join(out_dir, 'article_question_generation_train_data.csv')
-    clean_data_val_out_file = os.path.join(out_dir, 'article_question_generation_val_data.csv')
+    clean_data_train_out_file = os.path.join(out_dir, f'{data_name}_train_data.csv')
+    clean_data_val_out_file = os.path.join(out_dir, f'{data_name}_val_data.csv')
     clean_data_train.to_csv(clean_data_train_out_file, sep=',', index=False)
     clean_data_val.to_csv(clean_data_val_out_file, sep=',', index=False)
     # reload data into correct format lol
@@ -36,11 +36,12 @@ def prepare_question_data(data, out_dir, tokenizer, train_pct=0.8):
     val_data_set = nlp.load_dataset('csv', data_files=clean_data_val_out_file)
     #     tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
     # get max lengths
-    source_text_tokens = clean_data.loc[:, 'source_text'].apply(lambda x: tokenizer.tokenize(x))
-    target_text_tokens = clean_data.loc[:, 'target_text'].apply(lambda x: tokenizer.tokenize(x))
+    # source_text_tokens = clean_data.loc[:, 'source_text'].apply(lambda x: tokenizer.tokenize(x))
+    # target_text_tokens = clean_data.loc[:, 'target_text'].apply(lambda x: tokenizer.tokenize(x))
     #     max_source_length = max(source_text_tokens.apply(lambda x: len(x)))
     #     max_target_length = max(target_text_tokens.apply(lambda x: len(x)))
     # tmp debugging: shorten source/target
+    ## TODO: increase max article length!!
     max_source_length = 1024
     max_target_length = 64
     data_processor = DataProcessor(tokenizer=tokenizer,
@@ -53,13 +54,52 @@ def prepare_question_data(data, out_dir, tokenizer, train_pct=0.8):
     train_data.set_format(type='torch', columns=columns)
     val_data.set_format(type='torch', columns=columns)
     #     print(f'train data {train_data}')
-    train_data_out_file = os.path.join(out_dir, 'article_question_generation_train_data.pt')
-    val_data_out_file = os.path.join(out_dir, 'article_question_generation_val_data.pt')
+    train_data_out_file = os.path.join(out_dir, f'{data_name}_train_data.pt')
+    val_data_out_file = os.path.join(out_dir, f'{data_name}_val_data.pt')
     torch.save(train_data, train_data_out_file)
     torch.save(val_data, val_data_out_file)
     # save tokenizer?? sure
     tokenizer_out_file = os.path.join(out_dir, 'BART_tokenizer.pt')
     torch.save(tokenizer, tokenizer_out_file)
+
+def load_training_args(model_out_dir, train_data_file, val_data_file, out_dir, max_source_len, max_target_len):
+    training_args = DataArguments(model_out_dir)
+    training_args.train_file_path = train_data_file
+    training_args.valid_file_path = val_data_file
+    training_args.data_dir = out_dir
+    training_args.task = 'qg'
+    training_args.max_source_length = max_source_len
+    training_args.max_target_length = max_target_len
+    training_args.n_gpu = 1
+    # training_args.device = 'cuda:1'
+    training_args.seed = 123
+    training_args.disable_tqdm = False
+    training_args.local_rank = -1
+    training_args.output_dir = model_out_dir
+    training_args.num_train_epochs = 20
+    # training_args.max_steps = 1
+    training_args.fp16 = False
+    training_args.label_names = None
+    training_args.per_device_train_batch_size = 2
+    training_args.per_device_eval_batch_size = 2
+    # training_args.train_batch_size = 32
+    # training_args.eval_batch_size = 32
+    training_args.gradient_accumulation_steps = 4
+    training_args.learning_rate = 1e-4
+    training_args.dataloader_drop_last = False
+    training_args.dataloader_num_workers = 8
+    training_args.evaluate_during_training = True
+    training_args.do_eval = True
+    training_args.evaluation_strategy = 'epoch'
+    # default values from here lol https://github.com/huggingface/transformers/blob/49759c0cda29ab614b81e0869972c99f2edba7aa/src/transformers/training_args.py
+    training_args.weight_decay = 0.01
+    training_args.adam_beta1 = 0.9
+    training_args.adam_beta2 = 0.999
+    training_args.adam_epsilon = 1e-8
+    training_args.warmup_steps = 0
+    # limits number of checkpoints => 1 GB per optimizer file ;_;
+    training_args.save_total_limit = 2
+    return training_args
 
 from argparse import ArgumentParser
 
@@ -77,18 +117,18 @@ def main():
 
     ## prepare data
     article_question_data = pd.read_csv(raw_train_data_file, sep='\t', index_col=False)
-
     train_pct = 0.8
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
     # TODO: increase vocab size to include named entities?
     # TODO: shrink data to debug training
     # tmp debugging
-    article_question_data = article_question_data.copy().head(2000)
-    train_data_file = os.path.join(out_dir, 'article_question_generation_train_data.pt')
-    val_data_file = os.path.join(out_dir, 'article_question_generation_val_data.pt')
+    # article_question_data = article_question_data.copy().head(2000)
+    data_name = os.path.basename(raw_train_data_file).replace('.tsv', '')
+    train_data_file = os.path.join(out_dir, f'{data_name}_train_data.pt')
+    val_data_file = os.path.join(out_dir, f'{data_name}_val_data.pt')
     if(not os.path.exists(train_data_file)):
-        prepare_question_data(article_question_data, out_dir, tokenizer=tokenizer,
-                              train_pct=train_pct)
+        prepare_question_data(article_question_data, out_dir, data_name,
+                              tokenizer=tokenizer, train_pct=train_pct)
 
     ## train model
     cache_dir = os.path.join(out_dir, 'model_cache/')
@@ -125,44 +165,12 @@ def main():
     model_out_dir = os.path.join(out_dir, 'question_generation_model/')
     if (not os.path.exists(model_out_dir)):
         os.mkdir(model_out_dir)
-    training_args = DataArguments(model_out_dir)
-    training_args.train_file_path = train_data_file
-    training_args.valid_file_path = val_data_file
-    training_args.data_dir = out_dir
-    training_args.task = 'qg'
-    training_args.max_source_length = max_source_len
-    training_args.max_target_length = max_target_len
-    training_args.n_gpu = 1
-    # training_args.device = 'cuda:1'
-    training_args.seed = 123
-    training_args.disable_tqdm = False
-    training_args.local_rank = -1
-    training_args.output_dir = model_out_dir
-    training_args.num_train_epochs = 20
-    # training_args.max_steps = 1
-    training_args.fp16 = False
-    training_args.label_names = None
-    training_args.per_device_train_batch_size = 4
-    training_args.per_device_eval_batch_size = 4
-    # training_args.train_batch_size = 32
-    # training_args.eval_batch_size = 32
-    training_args.gradient_accumulation_steps = 4
-    training_args.learning_rate = 1e-4
-    training_args.dataloader_drop_last = False
-    training_args.dataloader_num_workers = 8
-    # default values from here lol https://github.com/huggingface/transformers/blob/49759c0cda29ab614b81e0869972c99f2edba7aa/src/transformers/training_args.py
-    training_args.weight_decay = 0.01
-    training_args.adam_beta1 = 0.9
-    training_args.adam_beta2 = 0.999
-    training_args.adam_epsilon = 1e-8
-    training_args.warmup_steps = 0
-    # limits number of checkpoints => 1 GB per optimizer file ;_;
-    training_args.save_total_limit = 2
+
+    training_args = load_training_args(model_out_dir, train_data_file, model_out_dir, val_data_file, max_source_len, max_target_len)
     model_args = {
         'label_smoothing': 0,
     }
     ## TODO: prevent model from saving optimizer during every 500 training steps!!
-    ## TODO: log train/val loss in same way
     trainer = Trainer(
         model=model,
         args=training_args,
--- scripts/data_processing/train_basic_question_generation.sh
+++ scripts/data_processing/train_basic_question_generation.sh
@@ -1,5 +1,9 @@
-TRAIN_DATA=../../data/CNN_articles/cnn/article_question_data.tsv
-OUT_DIR=../../data/CNN_articles/cnn/
+## CNN data
+#TRAIN_DATA=../../data/CNN_articles/cnn/article_question_data.tsv
+#OUT_DIR=../../data/CNN_articles/cnn/
+## NYT data
+TRAIN_DATA=../../data/nyt_comments/NYT_question_data.tsv
+OUT_DIR=../../data/nyt_comments/
 DEVICE="cuda:0"
 MODEL_TYPE="bart"
 python train_basic_question_generation.py $TRAIN_DATA $OUT_DIR --device $DEVICE --model_type $MODEL_TYPE
