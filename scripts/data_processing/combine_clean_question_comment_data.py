0de4b8760561c33991f3314b1be45840de4828c1
ianbstewart <ibs@dartmouth.edu>
2021-03-24 20:52:06 +0000

    spaghetti code for collecting prior posts from authors

--- scripts/data_processing/combine_clean_question_comment_data.py
+++ scripts/data_processing/combine_clean_question_comment_data.py
@@ -14,58 +14,58 @@ import numpy as np
 
 def filter_comments_by_post_overlap(comment_data, post_data_file):
     post_data = load_zipped_json_data(post_data_file)
-   post_data.rename(columns={
+    post_data.rename(columns={
         'id': 'parent_id', 'created_utc': 'parent_created',
         'selftext': 'parent_text', 'title': 'parent_title',
         'edited': 'parent_edited', 'author': 'parent_author'
     }, inplace=True)
-   # remove edits
+    # print(f'post IDs {post_data.loc[:, "parent_id"].unique()[:10]}')
+    # print(f'comment parent IDs {comment_data.loc[:, "parent_id"].unique()[:10]}')
+    # remove edits
     post_data = post_data[post_data.loc[:, 'parent_edited'].apply(
         lambda x: type(x) is bool and not x)]
     ## combine with questions
     post_cols = ['parent_id', 'parent_created', 'parent_text', 'parent_title',
                  'parent_edited', 'parent_author']
-   # print(f'comment data cols {comment_data.columns}')
-    comment_post_data = pd.merge(comment_data, post_data.loc[:, post_cols],
+    # print(f'comment data cols {comment_data.columns}')
+    post_data = pd.merge(comment_data, post_data.loc[:, post_cols],
                                  on='parent_id')
-    print(f'{comment_post_data.shape[0]}/{comment_data.shape[0]} comments retained after merge with posts')
+
+    print(f'{post_data.shape[0]}/{comment_data.shape[0]} comments retained after merge with posts')
     # get sentences/tokens
     word_tokenizer = WordPunctTokenizer()
     sent_tokenizer = PunktSentenceTokenizer()
     stemmer = PorterStemmer()
-    comment_post_data = comment_post_data.assign(**{
-        'parent_sents': comment_post_data.loc[:, 'parent_text'].apply(
+    post_data = post_data.assign(**{
+        'parent_sents': post_data.loc[:, 'parent_text'].apply(
             lambda x: tokenize_stem_text(x, stemmer, word_tokenizer,
                                          sent_tokenizer)),
-        'question_sents': comment_post_data.loc[:,
+        'question_sents': post_data.loc[:,
                           'question'].apply(
             lambda x: tokenize_stem_text(x, stemmer, word_tokenizer,
                                          sent_tokenizer))
     })
     ## compute overlap
-    comment_post_data = comment_post_data.assign(**{
-        'post_question_overlap': comment_post_data.apply(
+    print(f'compute post/question overlap')
+    post_data = post_data.assign(**{
+        'post_question_overlap': post_data.progress_apply(
             lambda x: compute_sent_word_overlap(x.loc['parent_sents'],
                                                 x.loc['question_sents']),
             axis=1)
     })
-    comment_post_data = comment_post_data.assign(**{
-        'post_question_overlap_score': comment_post_data.loc[:,
+    post_data = post_data.assign(**{
+        'post_question_overlap_score': post_data.loc[:,
                                        'post_question_overlap'].apply(
             lambda x: x[0]),
-        'post_question_overlap_sent': comment_post_data.loc[:,
+        'post_question_overlap_sent': post_data.loc[:,
                                       'post_question_overlap'].apply(
             lambda x: x[1][0]),
     })
     # restrict to overlap [0.1, 0.5] based on earlier tests
     overlap_score_range = [0.1, 0.5]
-    valid_overlap_comment_data = comment_post_data[(comment_post_data.loc[:,
-                                                    'post_question_overlap_score'] >=
-                                                    overlap_score_range[0]) &
-                                                   (comment_post_data.loc[:,
-                                                    'post_question_overlap_score'] <
-                                                    overlap_score_range[1])]
-    valid_comment_ids = valid_overlap_comment_data.loc[:, 'id']
+    valid_overlap_comment_data = post_data[(post_data.loc[:,'post_question_overlap_score'] >= overlap_score_range[0]) &
+                                           (post_data.loc[:, 'post_question_overlap_score'] < overlap_score_range[1])]
+    # valid_comment_ids = valid_overlap_comment_data.loc[:, 'id']
     comment_data = pd.merge(
         comment_data,
         valid_overlap_comment_data.loc[:,
@@ -75,7 +75,7 @@ def filter_comments_by_post_overlap(comment_data, post_data_file):
     return comment_data
 
 def filter_comments_by_valid_question_prob(comment_data, model_file):
-   valid_question_model = pickle.load(open(model_file, 'rb'))
+    valid_question_model = pickle.load(open(model_file, 'rb'))
     vocab_file = model_file.replace('.pkl', '_vocab.txt')
     model_vocab = list(map(lambda x: x.strip(), open(vocab_file, 'r')))
     cv = CountVectorizer(vocabulary=model_vocab)
@@ -115,6 +115,7 @@ def main():
     # comment_data = comment_data.iloc[:10000, :]
 
     ## extract questions
+    print(f'extracting questions')
     min_question_len = 5
     comment_data = comment_data.assign(**{
         'questions' : extract_questions_all_data(comment_data.loc[:, 'body'], min_question_len=min_question_len)
@@ -141,6 +142,7 @@ def main():
 
     ## filter for post overlap
     if(args.get('post_data') is not None):
+        print(f'computing post overlap')
         comment_data = filter_comments_by_post_overlap(comment_data, args['post_data'])
     ## filter for valid clarification questions
     if(args.get('valid_question_model') is not None):
