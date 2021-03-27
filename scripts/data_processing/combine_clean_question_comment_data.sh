bc1b687cdf6ecf9637dff6c9e91e4a0be1248fae
ianbstewart <ibs@dartmouth.edu>
2021-03-27 22:15:50 +0000

    tested subreddit embeddings for different times

--- scripts/data_processing/combine_clean_question_comment_data.sh
+++ scripts/data_processing/combine_clean_question_comment_data.sh
@@ -3,7 +3,7 @@ DATA_NAME=advice_subreddit
 POST_DATA=../../data/reddit_data/subreddit_submissions_2018-01_2019-12.gz
 VALID_QUESTION_MODEL=../../data/reddit_data/valid_question_detection_model.pkl
 #python combine_clean_question_comment_data.py $DATA_DIR --post_data $POST_DATA --valid_question_model $VALID_QUESTION_MODEL
-MAX_MEMORY=30000000000 # 20G?
+MAX_MEMORY=50000000000 # 20G?
 python combine_clean_question_comment_data.py $DATA_DIR --post_data $POST_DATA --valid_question_model $VALID_QUESTION_MODEL &
 PID=$!
 prlimit --pid $PID --as=$MAX_MEMORY
