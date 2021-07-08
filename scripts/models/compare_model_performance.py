ac221f4d94da2da827fba0e208a715f811c075d8
ianbstewart <ianbstew@lit1000.eecs.umich.edu>
2021-07-08 15:11:44 -0400

    fixed model significance testing; still fixing data train/test split

--- scripts/models/compare_model_performance.py
+++ scripts/models/compare_model_performance.py
@@ -37,7 +37,7 @@ def main():
     model_output_data = pd.DataFrame(model_output_data, index=model_names).transpose()
     model_output_data = model_output_data.assign(**{'target_text' : test_text_data})
     # tmp debugging
-    model_output_data = model_output_data.iloc[:1000, :]
+    # model_output_data = model_output_data.loc[np.random.choice(model_output_data.index, 1000, replace=False), :]
 
     ## get generation scores...again
     ## overlap scores => BLEU, ROUGE, WMD, sentence distance
@@ -59,11 +59,11 @@ def main():
     train_data_text = train_data['target_text']
     model_cache_dir = '../../data/model_cache/'
     model_type_lookup = {
-        'text_only_data' : 'bart',
-        'author_text_data' : 'bart',
-        'author_attention_data' : 'bart_author_attention',
-        'author_subreddit_embed_data' : 'bart_author_embeds',
-        'author_text_embed_data': 'bart_author_embeds',
+        'text' : 'bart',
+        'reader_token' : 'bart',
+        'reader_attention' : 'bart_author_attention',
+        'reader_subreddit_embed' : 'bart_author_embeds',
+        'reader_text_embed': 'bart_author_embeds',
     }
     test_data_dir = os.path.dirname(test_data_file)
     perplexity_sample_size = 5000
@@ -73,24 +73,26 @@ def main():
     for model_name_i, model_output_file_i in zip(model_names, model_output_files):
         # redundancy
         redundancy_i = model_output_data.loc[:, model_name_i].apply(lambda x: int(x in train_data_text))
-        redundancy_i = pd.DataFrame(redundancy_i, index=['redundancy']).assign(**{'model' : model_name_i})
+        redundancy_i = pd.DataFrame(redundancy_i, index=['redundancy']).transpose().assign(**{'model_name' : model_name_i})
         redundancy_data.append(redundancy_i)
         # perplexity
         # reload original model
         model_output_file_dir_i = os.path.join(os.path.dirname(model_output_file_i), 'question_generation_model')
-        print(f'model output file dir {model_output_file_dir_i} has files {os.listdir(model_output_file_dir_i)}')
+        # print(f'model output file dir {model_output_file_dir_i} has files {os.listdir(model_output_file_dir_i)}')
         model_output_file_dir_files_i = list(map(lambda x: os.path.join(model_output_file_dir_i, x), os.listdir(model_output_file_dir_i)))
         model_checkpoint_dirs_i = list(filter(lambda x: os.path.basename(x).startswith('checkpoint-') and os.path.isdir(x), model_output_file_dir_files_i))
-        print(f'model checkpoint dirs = {model_checkpoint_dirs_i}')
+        # print(f'model checkpoint dirs = {model_checkpoint_dirs_i}')
         most_recent_checkpoint_dir_i = max(model_checkpoint_dirs_i, key=lambda x: int(os.path.basename(x).replace('checkpoint-', '')))
         model_weight_file_i = os.path.join(most_recent_checkpoint_dir_i, 'pytorch_model.bin')
         model_type_i = model_type_lookup[model_name_i]
         model_i, tokenizer_i = load_model(model_cache_dir, model_weight_file_i, model_type_i, test_data_dir)
         log_likelihoods_i, _ = compute_perplexity(model_i, model_type_i, perplexity_sample_size, test_data_sample, return_log_likelihoods=True)
-        perplexity_i = pd.DataFrame(log_likelihoods_i, index=['perplexity']).assign(**{'model' : model_name_i})
+        perplexity_i = pd.DataFrame(log_likelihoods_i, columns=['perplexity']).assign(**{'model_name' : model_name_i})
         perplexity_data.append(perplexity_i)
     redundancy_data = pd.concat(redundancy_data, axis=0)
     perplexity_data = pd.concat(perplexity_data, axis=0)
+    # tmp debugging
+    # print(f'perplexity data sample {perplexity_data.head(10)}')
 
     ## test significance
     model_combos = list(combinations(model_names, 2))
@@ -101,7 +103,10 @@ def main():
     model_score_data = []
     for model_1, model_2 in model_combos:
         generation_score_data_1 = generation_score_data[generation_score_data.loc[:, 'model_name']==model_1]
-        generation_score_data_2 = generation_score_data[generation_score_data.loc[:, 'model_name'] == model_2]
+        generation_score_data_2 = generation_score_data[generation_score_data.loc[:, 'model_name']==model_2]
+        # tmp debugging
+        # print(f'score data 1 sample = {generation_score_data_1.loc[:, "BLEU-1"].head(50)}')
+        # print(f'score data 2 sample = {generation_score_data_2.loc[:, "BLEU-1"].head(50)}')
         for generation_score_var_i in generation_score_vars:
             mean_diff_i = np.mean(generation_score_data_1.loc[:, generation_score_var_i] - generation_score_data_2.loc[:, generation_score_var_i])
             test_stat, p_val = wilcoxon(generation_score_data_1.loc[:, generation_score_var_i], generation_score_data_2.loc[:, generation_score_var_i])
@@ -118,12 +123,12 @@ def main():
         redundancy_data_2 = redundancy_data[redundancy_data.loc[:, 'model_name']==model_2]
         redundancy_diffs = []
         for _ in range(bootstrap_iters):
-            bootstrap_idx_i = np.random.choice(list(range(redundancy_data_1.shape[0])), bootstrap_sample_size, replace=False)
+            bootstrap_idx_i = np.random.choice(list(range(redundancy_data_1.shape[0])), bootstrap_sample_size, replace=(bootstrap_sample_size > redundancy_data_1.shape[0]))
             redundancy_data_1_i = redundancy_data_1.iloc[bootstrap_idx_i, :]
             redundancy_data_2_i = redundancy_data_2.iloc[bootstrap_idx_i, :]
             redundancy_diff_i = redundancy_data_1_i.loc[:, 'redundancy'].mean() - redundancy_data_2_i.loc[:, 'redundancy'].mean()
             redundancy_diffs.append(redundancy_diff_i)
-        redundancy_diff = np.median(redundancy_diff)
+        redundancy_diff = np.mean(redundancy_diffs)
         test_stat, p_val = sign_test(redundancy_diff, mu0=0.)
         model_score_data.append([model_1, model_2, 'redundancy', redundancy_diff, test_stat, p_val])
         # diversity => bootstrap
@@ -131,12 +136,12 @@ def main():
         model_text_2 = model_output_data.loc[:, model_2]
         diversity_diffs = []
         for _ in range(bootstrap_iters):
-            bootstrap_idx_i = np.random.choice(list(range(model_output_data.shape[0])), bootstrap_sample_size, replace=False)
+            bootstrap_idx_i = np.random.choice(list(range(model_output_data.shape[0])), bootstrap_sample_size, replace=(bootstrap_sample_size > model_output_data.shape[0]))
             diversity_1_i = model_text_1.iloc[bootstrap_idx_i].nunique() / model_text_1.shape[0]
             diversity_2_i = model_text_2.iloc[bootstrap_idx_i].nunique() / model_text_2.shape[0]
             diversity_diff_i = diversity_1_i - diversity_2_i
             diversity_diffs.append(diversity_diff_i)
-        diversity_diff = np.median(diversity_diffs)
+        diversity_diff = np.mean(diversity_diffs)
         test_stat, p_val = sign_test(diversity_diff, mu0=0.)
         model_score_data.append([model_1, model_2, 'diversity', diversity_diff, test_stat, p_val])
     model_score_data = pd.DataFrame(model_score_data, columns=['model_1', 'model_2', 'score', 'mean_diff', 'test_stat', 'p'])
