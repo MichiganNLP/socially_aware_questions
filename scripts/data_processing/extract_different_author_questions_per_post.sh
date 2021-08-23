5ce622c1db750477bc37790d87f13a4b8c5ba4f9
ianbstewart <ibs@dartmouth.edu>
2021-08-23 12:57:08 -0400

    merging

--- scripts/data_processing/extract_different_author_questions_per_post.sh
+++ scripts/data_processing/extract_different_author_questions_per_post.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=extract_different_author_questions_per_post
+#SBATCH --mail-user=ianbstew@umich.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --time=10:00:00
+#SBATCH --account=mihalcea1
+#SBATCH --output=/home/%u/logs/%x-%j.log
+#SBATCH --nodes=1
+#SBATCH --partition=standard
+#SBATCH --mem-per-cpu=80g
+
 OUT_DIR=../../data/reddit_data/
 python extract_different_author_questions_per_post.py $OUT_DIR
