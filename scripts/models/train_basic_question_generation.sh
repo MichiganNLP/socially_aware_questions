809ebb195bdf5ebd963da83217eff66e3eae4939
ianbstewart <ibs@dartmouth.edu>
2021-10-01 15:28:53 -0400

    merging

--- scripts/models/train_basic_question_generation.sh
+++ scripts/models/train_basic_question_generation.sh
@@ -42,11 +42,11 @@
 #VAL_DATA=../../data/reddit_data/advice_subreddit_val_data.pt
 ## reddit+author data
 # "full" (sampled) data
-TRAIN_DATA=../../data/reddit_data/combined_data_train_data.pt
-VAL_DATA=../../data/reddit_data/combined_data_test_data.pt
+#TRAIN_DATA=../../data/reddit_data/combined_data_train_data.pt
+#VAL_DATA=../../data/reddit_data/combined_data_test_data.pt
 # split training data (parameter tuning)
-#TRAIN_DATA=../../data/reddit_data/combined_data_train_train_data.pt
-#VAL_DATA=../../data/reddit_data/combined_data_train_val_data.pt
+TRAIN_DATA=../../data/reddit_data/combined_data_train_train_data.pt
+VAL_DATA=../../data/reddit_data/combined_data_train_val_data.pt
 # author-only data: fine-tuning
 #TRAIN_DATA=../../data/reddit_data/combined_data_valid_authors_train_data.pt
 #VAL_DATA=../../data/reddit_data/combined_data_valid_authors_test_data.pt
@@ -76,12 +76,12 @@ VAL_DATA=../../data/reddit_data/combined_data_test_data.pt
 #MODEL_TYPE="bart_author_attention"
 #MODEL_CONFIG_FILE=../../data/model_cache/BART_author_attention_model_config.json
 # author attention: hyperparameter tests
-#OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_location=encoder_config=attnconcat/
+OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_location=encoder_config=attnconcat/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=3_location=encoder_config=attnconcat/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=5_location=encoder_config=attnconcat/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_location=decoder_config=attnconcat/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=3_location=decoder_config=attnconcat/
-OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=5_location=decoder_config=attnconcat/
+#OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=5_location=decoder_config=attnconcat/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_weight=0.1_location=encoder_config=attnprob/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_weight=0.1_location=encoder/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=1_weight=0.5_location=encoder/
@@ -102,7 +102,7 @@ OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_att
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=5_weight=0.5_location=decoder/
 #OUT_DIR=../../data/reddit_data/author_text_data/author_attention_data/author_attention_layer=5_weight=0.9_location=decoder/
 # optional hyperparameters: for overriding config file
-MODEL_CONFIG_PARAMS="reader_attn_position=5,reader_group_attention_location=decoder,reader_attn_config=attn_full"
+MODEL_CONFIG_PARAMS="reader_attn_position=1,reader_group_attention_location=encoder,reader_attn_config=attn_full"
 MODEL_TYPE="bart_author_attention"
 MODEL_CONFIG_FILE=../../data/model_cache/BART_author_attention_model_config.json
 ## author embed
