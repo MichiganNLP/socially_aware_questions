a4c96356a4c7847c34b04bd4c2c1e708f23f9971
istewart6 <istewart6@gatech.edu>
2021-10-21 18:25:41 -0400

    changed author group prediction to per-subreddit

--- scripts/models/test_author_group_prediction.sh
+++ scripts/models/test_author_group_prediction.sh
@@ -10,19 +10,19 @@
 #SBATCH --gpus=1
 
 #GROUP_CATEGORIES=("location_region" "expert_pct_bin" "relative_time_bin")
-GROUP_CATEGORIES=("relative_time_bin")
-TEXT_VAR="post_question"
+GROUP_CATEGORIES=("expert_pct_bin")
+#TEXT_VAR="post_question"
 #TEXT_VAR="question"
 # queue server
 # offline model because no internet connection in cluster
-export HF_DATASETS_OFFLINE=1
-export TRANSFORMERS_OFFLINE=1 
-python test_author_group_prediction.py --group_categories "${GROUP_CATEGORIES[@]}" --out_dir ../../data/reddit_data/group_classification_model/ --text_var $TEXT_VAR
+#export HF_DATASETS_OFFLINE=1
+#export TRANSFORMERS_OFFLINE=1
+#python test_author_group_prediction.py --group_categories "${GROUP_CATEGORIES[@]}" --out_dir ../../data/reddit_data/group_classification_model/
 
 # regular server
 # specify CUDA device (only for LIT machines)
-#export CUDA_VISIBLE_DEVICES=3
-#python test_author_group_prediction.py --group_categories "${GROUP_CATEGORIES[@]}" --out_dir ../../data/reddit_data/group_classification_model/
+export CUDA_VISIBLE_DEVICES=0
+python test_author_group_prediction.py --group_categories "${GROUP_CATEGORIES[@]}" --out_dir ../../data/reddit_data/group_classification_model/
 # dumb parallel code that doesn't work fml
 #export LOCAL_RANK=1
 #python -m torch.distributed.launch --nproc_per_node 2 --use_env test_author_group_prediction.py "${GROUP_CATEGORIES[@]}"
